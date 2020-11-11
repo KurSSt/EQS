1774cdda3abb00343ac278c9a71ff98bb05a37cc
wojciechlaszczak <51190158+wojciechlaszczak@users.noreply.github.com>
2020-11-11 10:27:18 +0100

    First Commit

---
+++ EQSoft_Testing/EqSoft/NativeMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace EqSoft
+{
+    class NativeMethods
+    {
+        const string Magnification = "Magnification.dll";
+
+        [DllImport(Magnification, ExactSpelling = true, SetLastError = true)]
+        public static extern bool MagInitialize();
+
+        [DllImport(Magnification, ExactSpelling = true, SetLastError = true)]
+        public static extern bool MagUninitialize();
+
+        [DllImport("user32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        public static extern bool SetMagnificationDesktopColorEffect(ref MAGCOLOREFFECT pEffect);
+
+        [DllImport(Magnification, ExactSpelling = true, SetLastError = true)]
+        public static extern bool MagSetFullscreenColorEffect(ref MAGCOLOREFFECT pEffect);
+        public struct MAGCOLOREFFECT
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 25)]
+            public float[] transform;
+        }
+    }
+}
